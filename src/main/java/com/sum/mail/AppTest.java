d2171b56e1309fab4027b85943750c96170d0577
Jeferson Nascimeto <mjef013@gmail.com>
2024-10-11 11:56:20 -0300

    Parâmetros de conexão com Gmails;

--- src/main/java/com/sum/mail/AppTest.java
+++ src/main/java/com/sum/mail/AppTest.java
@@ -1,10 +1,36 @@
 package com.sum.mail;
 
 
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
+
 public class AppTest {
 
     @org.junit.Test
     public void testeEmail(){
+        Properties properties = new Properties();
+
+        /*Paramêtros para conexão com Gmail*/
+
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port","465");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", "465");
+
+        Session session = Session.getInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("raram360@gmail.com",
+                                "2001Xjtao");
+                    }
+                });
+
+        /* Ativa Debug para a sessão */
+        session.setDebug(true);
+
+
 
     }
 
